7142659062d20584c54a4a73e73a953eb1aa50c7
Rajorshi Chaudhuri <rajorshi01@gmail.com>
2018-08-11 22:43:58 +0530

    Update dcgen.py

--- models/dcgen.py
+++ models/dcgen.py
@@ -12,6 +12,24 @@ class DCGenerator(nn.Module):
           z: latent variable vector size
           c: number of channels
           ngf: size of generator features
+          DCGenerator(
+            (main): Sequential(
+            (0): ConvTranspose2d(100, 512, kernel_size=(4, 4), stride=(1, 1), bias=False)
+            (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            (2): ReLU(inplace)
+            (3): ConvTranspose2d(512, 256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+            (4): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            (5): ReLU(inplace)
+            (6): ConvTranspose2d(256, 128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+            (7): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            (8): ReLU(inplace)
+            (9): ConvTranspose2d(128, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+            (10): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            (11): ReLU(inplace)
+            (12): ConvTranspose2d(64, 3, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+            (13): Tanh()
+            )
+          )
         '''
         super(DCGenerator, self).__init__()
         self.model = nn.Sequential(
