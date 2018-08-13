962048473c1e54f941acf8c0df4a612bf95811e3
Rajorshi Chaudhuri <rajorshi01@gmail.com>
2018-08-14 00:10:57 +0530

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from torch.nn.parameter import Parameter
 import torch.nn.init as init
 import torch.nn.functional as F
 
-def train(G, D_set, optimG, optimD, dataloader, criterion, N, T_max, T_w=None, alpha=0.1, _lambda=15):
+def train(G, D_set, optimG, optimD, dataloader, criterion, z, N, T_max, T_w=None, alpha=0.1, _lambda=15):
   '''
     G: generator
     D_set: set of discriminators
@@ -21,6 +21,7 @@ def train(G, D_set, optimG, optimD, dataloader, criterion, N, T_max, T_w=None, a
     optimD: set of discriminator optimiser
     dataloader: dataloader
     criterion: loss function
+    z: latent vector size
     T_max: max time steps
     T_w: warm up time
     alpha:moving average coefficient
@@ -38,7 +39,10 @@ def train(G, D_set, optimG, optimD, dataloader, criterion, N, T_max, T_w=None, a
 
   for t in range(T_max):
     
-    G_prev = G
+    # G_prev eval
+    eval_noise = torch.randn(128, z, 1, 1, device=device)
+    eval_fake = G(eval_noise)
+    
     for i, data in enumerate(dataloader, 0):
       
       # real and fake labels
@@ -59,7 +63,7 @@ def train(G, D_set, optimG, optimD, dataloader, criterion, N, T_max, T_w=None, a
         errD_real.backward()
         
         # train with fake
-        noise = torch.randn(batch_size, nz, 1, 1, device=device)
+        noise = torch.randn(batch_size, z, 1, 1, device=device)
         fake = G(noise)
         label.fill_(fake_label)
         output = D(fake.detach())
@@ -85,23 +89,22 @@ def train(G, D_set, optimG, optimD, dataloader, criterion, N, T_max, T_w=None, a
     if t >= T_w:
       
       real_label = random.uniform(0.7, 1.2)
-      label = torch.full((batch_size,), real_label, device=device)
+      label = torch.full((128,), real_label, device=device)
       
-      # choose arm k according to 7
-      k = np.random.choice(N, 1, p=pi)
+      # choose arm k 
+      k = np.random.choice(N, 1, p=pi.cpu().numpy())[0]
       
       # evaluate performance of G with D_k
-      noise = torch.randn(batch_size, nz, 1, 1, device=device)
-      fake = G(noise)
-      output = D_set[k](fake)
+      fake = G(eval_noise)
+      output = D_set[k](fake.detach())
       
-      # receive reward among 4, 5
-      output_prev_G = D_set[k](G_prev(noise))
+      # receive reward
+      output_prev_G = D_set[k](eval_fake.detach())
       reward = criterion(output, label) - criterion(output_prev_G, label)
-      
-      # update Q and pi values according to 6, 7
+    
+      # update Q and pi values 
       Q[k] = alpha * reward + (1 - alpha) * Q[k]
-      pi = F.softmax(_lambda * Q)
+      pi = F.softmax(_lambda * Q).detach().to(device)
       
   return losses_D, losses_G
     
